f14c98bd4e6e25c05117b8229af21a57c8290ef5
Creativeguru97 <worldapple2630@gmail.com>
2019-04-02 05:50:13 +0900

    April 1, 2019

---
+++ C9/NOC_9_01_ShakespeareAcceptOrRejectP5/sketch.js
@@ -0,0 +1,250 @@
+
+let target;
+let popmax;
+let mutationRate;
+let population;
+
+let bestPhrase;
+let allPhrases;
+let stats;
+
+function setup() {
+  bestPhrase = createP("Best phrase:");
+  //bestPhrase.position(10,10);
+  bestPhrase.class("best");
+
+  allPhrases = createP("All phrases:");
+  allPhrases.position(600, 10);
+  allPhrases.class("all");
+
+  stats = createP("Stats");
+  //stats.position(10,200);
+  stats.class("stats");
+
+  //createCanvas(640, 360);
+  target = "To be or not to be.";
+  popmax = 200;
+  mutationRate = 0.01;
+
+  // Create a population with a target phrase, mutation rate, and population max
+  population = new Population(target, mutationRate, popmax);
+}
+
+function draw() {
+  //Create next generation
+  population.generate();
+  // Calculate fitness
+  population.calcFitness();
+
+  population.evaluate();
+
+  // If we found the target phrase, stop
+  if (population.isFinished()) {
+    //println(millis()/1000.0);
+    noLoop();
+  }
+
+  displayInfo();
+}
+
+function displayInfo() {
+  // Display current status of population
+  let answer = population.getBest();
+
+  bestPhrase.html("Best phrase:<br>" + answer);
+
+  let statstext = "total generations:     " + population.getGenerations() + "<br>";
+  statstext += "average fitness:       " + nf(population.getAverageFitness()) + "<br>";
+  statstext += "total population:      " + popmax + "<br>";
+  statstext += "mutation rate:         " + floor(mutationRate * 100) + "%";
+
+  stats.html(statstext);
+
+  allPhrases.html("All phrases:<br>" + population.allPhrases())
+}
+
+
+
+//Because I dont know how to caonnect this main file to others, I write here.
+class Population {
+  constructor(p, m, num) {
+
+    this.population; // Array to hold the current population
+    this.matingPool; // ArrayList which we will use for our "mating pool"
+    this.generations = 0; // Number of generations
+    this.finished = false; // Are we finished evolving?
+    this.target = p; // Target phrase
+    this.mutationRate = m; // Mutation rate
+    this.perfectScore = 1;
+
+    this.best = "";
+
+    this.population = [];
+    for (let i = 0; i < num; i++) {
+      this.population[i] = new DNA(this.target.length);
+    }
+    this.matingPool = [];
+    this.calcFitness();
+  }
+
+  // Fill our fitness array with a value for every member of the population
+  calcFitness() {
+    for (let i = 0; i < this.population.length; i++) {
+      this.population[i].calcFitness(target);
+    }
+  }
+
+  // Create a new generation
+  generate() {
+    let maxFitness = 0;
+    for (let i = 0; i < this.population.length; i++) {
+      if (this.population[i].fitness > maxFitness) {
+        maxFitness = this.population[i].fitness;
+      }
+    }
+    // Refill the population with children from the mating pool
+    var newPopulation = [];
+    for (let i = 0; i < this.population.length; i++) {
+      let partnerA = this.acceptReject(maxFitness);
+      let partnerB = this.acceptReject(maxFitness);
+      let child = partnerA.crossover(partnerB);
+      child.mutate(this.mutationRate);
+      newPopulation[i] = child;
+    }
+    this.population = newPopulation;
+    this.generations++;
+  }
+
+acceptReject(maxVal){
+  var besafe = 0;
+  while(true){
+    var index = floor(random(this.population.length));
+    var partner = this.population[index];
+    var randomValue = random(maxVal);
+    if(randomValue < partner.fitness){
+      return partner;
+    }
+    besafe++;
+
+    if(besafe > 10000){
+      return null;
+    }
+  }
+}
+
+
+  getBest() {
+    return this.best;
+  }
+
+  // Compute the current "most fit" member of the population
+  evaluate() {
+    let worldrecord = 0.0;
+    let index = 0;
+    for (let i = 0; i < this.population.length; i++) {
+      if (this.population[i].fitness > worldrecord) {
+        index = i;
+        worldrecord = this.population[i].fitness;
+      }
+    }
+
+    this.best = this.population[index].getPhrase();
+    if (worldrecord >= this.perfectScore) {//Because we added 0.01 to highist score
+      this.finished = true;
+    }
+  }
+
+  isFinished() {
+    return this.finished;
+  }
+
+  getGenerations() {
+    return this.generations;
+  }
+
+  // Compute average fitness for the population
+  getAverageFitness() {
+    let total = 0;
+    for (let i = 0; i < this.population.length; i++) {
+      total += this.population[i].fitness;
+    }
+    return total / (this.population.length);
+  }
+
+  allPhrases() {
+    let everything = "";
+
+    let displayLimit = min(this.population.length, 50);
+
+
+    for (let i = 0; i < displayLimit; i++) {
+      everything += this.population[i].getPhrase() + "<br>";
+    }
+    return everything;
+  }
+}
+
+
+
+
+
+function newChar() {
+  let c = floor(random(63, 122));
+  if (c === 63) c = 32;
+  if (c === 64) c = 46;
+
+  return String.fromCharCode(c);
+}
+
+// Constructor (makes a random DNA)
+class DNA {
+  constructor(num) {
+    // The genetic sequence
+    this.genes = [];
+    this.fitness = 0;
+    for (let i = 0; i < num; i++) {
+      this.genes[i] = newChar(); // Pick from range of chars
+    }
+  }
+
+  // Converts character array to a String
+  getPhrase() {
+    return this.genes.join("");
+  }
+
+  // Fitness function (returns floating point % of "correct" characters)
+  calcFitness(target) {
+    let score = 0;
+    for (let i = 0; i < this.genes.length; i++) {
+      if (this.genes[i] == target.charAt(i)) {
+        score++;
+      }
+    }
+    this.fitness = score / target.length;
+    this.fitness = pow(this.fitness, 2)+0.01; //Make the fitness function exponential.
+  }
+
+  // Crossover
+  crossover(partner) {
+    // A new child
+    let child = new DNA(this.genes.length);
+
+    let midpoint = floor(random(this.genes.length)); // Pick a midpoint
+
+    // Half from one, half from the other
+    for (let i = 0; i < this.genes.length; i++) {
+      if (i > midpoint) child.genes[i] = this.genes[i];
+      else child.genes[i] = partner.genes[i];
+    }
+    return child;
+  }
+
+  // Based on a mutation probability, picks a new random character
+  mutate(mutationRate) {
+    for (let i = 0; i < this.genes.length; i++) {
+      if (random(1) < mutationRate) {
+        this.genes[i] = newChar();
+      }
+    }
+  }
+}
